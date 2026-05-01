deabdcfcd54c993723567c6157c3a903de300b2a
Vincent <vreynoso2@my.scccd.edu>
2026-05-01 23:56:40 +0000

    Lab practice

--- Reynoso14_2.m
+++ Reynoso14_2.m
@@ -1,1 +1,20 @@
-
+% Derivative of a Function
+% Set up an x-array from 0 to 10 with an increment of 0.01
+x = 0:0.01:10;
+y = 5*x.^2.*exp(-0.2*x).*sin(x);
+% Find the central difference estimate of the derivative
+n = length(x);
+der = (y(3:n)-y(1:n-2))./(x(3:n)-x(1:n-2));
+% Plot the original function 
+subplot(2,1,1)
+plot(x,y)
+grid on 
+title('Original fuction')
+xlabel("x")
+ylabel("y")
+subplot(2,1,2)
+plot(x(2:n-1),der,color='r')
+grid on 
+title('Derivative fuction')
+xlabel("x")
+ylabel("y")
